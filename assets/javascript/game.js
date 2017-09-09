5ec4c97a16ac021cd56b65d010682cf17c0bf664
scollins33 <sean_collins@gmx.com>
2017-09-08 21:12:53 -0400

    assignment completed, no bugs found

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,52 @@ var triviaGame = {
     timeLeft: 60,
     correctAnswers: 0,
     incorrectAnswers: 0,
-    unansweredQuestions: 0,
+    unansweredQuestions: 4,
     overallGrade: 0,
-    answerSheet: ['d', 'b', 'c', 'a'],
     winCondition: 'none',
+    questionTracker: 0,
+    quizSheet: {
+        question1: {
+            ques: 'How many F\'s are in this sentence: <br> FINISHED FILES ARE THE RESULT OF YEARS OF SCIENTIFIC STUDY.',
+            ans1: '2',
+            ans2: '3',
+            ans3: '4',
+            ans4: '5',
+            correctAnswer: 'd',
+            winGif: 'assets/images/congrats1.gif',
+            loseGif: 'assets/images/failure1.gif'
+        },
+        question2: {
+            ques: 'John has two coins that add up to $0.55, one is not a nickel. What two coins does he have?',
+            ans1: '2x quarters',
+            ans2: '1x half-dollar, 1x nickel',
+            ans3: '55x pennies',
+            ans4: 'None of the above',
+            correctAnswer: 'b',
+            winGif: 'assets/images/congrats2.gif',
+            loseGif: 'assets/images/failure2.gif'
+        },
+        question3: {
+            ques: 'If there are 6 apples on a table and you take 4, how many apples are there?',
+            ans1: '2',
+            ans2: '4',
+            ans3: '6',
+            ans4: '10',
+            correctAnswer: 'c',
+            winGif: 'assets/images/congrats3.gif',
+            loseGif: 'assets/images/failure3.gif'
+        },
+        question4: {
+            ques: 'If there are 12 fish and half of them drown, how many are there?',
+            ans1: '12',
+            ans2: '6',
+            ans3: '2',
+            ans4: '0',
+            correctAnswer: 'a',
+            winGif: 'assets/images/congrats4.gif',
+            loseGif: 'assets/images/failure4.gif'
+        }
+    },
 
     // set up initial game state
     setupGame: function () {
@@ -27,13 +69,31 @@ var triviaGame = {
         $('#quiz-start').addClass('hide-this');
         $('#quiz-body').removeClass('hide-this');
 
+        // create question array
+        var objKeys = Object.keys(triviaGame.quizSheet);
+
         // start timer
         quizInterval = setInterval(this.tickTimer, 1000);
 
-        // when the submit button is clicked, check all answers
-        $('#submit-button').on('click', function () {
-            triviaGame.winCondition = 'submitted';
-            triviaGame.checkAnswers();
+        // generate next question
+        triviaGame.nextQuestion(objKeys);
+    },
+
+    // inject text, wait for submit, check answer, react
+    nextQuestion: function (pKeys) {
+        // inject question & answer text
+        $('#gif-container').addClass('hide-this');
+        $('#answers-container').removeClass('hide-this');
+        $('#question-header').html(triviaGame.quizSheet[pKeys[this.questionTracker]].ques);
+        $('#ans1').text(triviaGame.quizSheet[pKeys[this.questionTracker]].ans1);
+        $('#ans2').text(triviaGame.quizSheet[pKeys[this.questionTracker]].ans2);
+        $('#ans3').text(triviaGame.quizSheet[pKeys[this.questionTracker]].ans3);
+        $('#ans4').text(triviaGame.quizSheet[pKeys[this.questionTracker]].ans4);
+
+        // when an answer button is clicked, check answers
+        $('.answer-button').on('click', function () {
+            $('.answer-button').off('click');
+            triviaGame.checkAnswer(this.value, pKeys);
         });
     },
 
@@ -42,56 +102,62 @@ var triviaGame = {
         $('#time-remaining').text(triviaGame.timeLeft);
 
         if (triviaGame.timeLeft === 0) {
-            triviaGame.checkAnswers();
+            triviaGame.winCondition = 'timeout';
+            triviaGame.showResults();
         }
     },
 
-    checkAnswers: function () {
-        var questionArray = ['q1-answer', 'q2-answer', 'q3-answer', 'q4-answer'];
-        var answerArray = [];
+    checkAnswer: function (pAnswer, pKeys) {
+        // decrease unanswered questions
+        this.unansweredQuestions--;
 
-        clearInterval(quizInterval);
-
-        // build answer array from form inputs
-        for (var i = 0; i < questionArray.length; i++) {
-            // generate answer list and set answered flag
-            var questionAnswers = document.getElementsByName(questionArray[i]);
-            var answered = false;
-
-            for (var j = 0; j < questionAnswers.length; j++) {
-                // run through each answer to find the selected one
-                if (questionAnswers[j].checked) {
-                    answerArray.push(questionAnswers[j].value);
-                    answered = true;
-                }
-            }
-            // if the questions isn't answered, mark it as none
-            if (answered === false) {
-                answerArray.push('none');
-            }
+        if (triviaGame.unansweredQuestions === 0) {
+            $('#next-button').text('Show Results');
         }
 
-        // compare answer array to answer sheet
-        for (var k = 0; k < answerArray.length; k++) {
-            if (answerArray[k] === 'none') {
-                this.unansweredQuestions++;
+        // compare answer to correct answer
+        // if you're right
+        if (pAnswer === this.quizSheet[pKeys[this.questionTracker]].correctAnswer) {
+            this.correctAnswers++;
+
+            // hide answers, fill gif, show gif
+            $('#answers-container').addClass('hide-this');
+            $('#gif-holder').html('<img src='+ this.quizSheet[pKeys[this.questionTracker]].winGif +' alt="correct!" />');
+            $('#gif-container').removeClass('hide-this');
+        }
+        //if you're wrong
+        else {
+            this.incorrectAnswers++;
+
+            // hide answers, fill gif, show gif
+            $('#answers-container').addClass('hide-this');
+            $('#gif-holder').html('<img src='+ this.quizSheet[pKeys[this.questionTracker]].loseGif +' alt="correct!" />');
+            $('#gif-container').removeClass('hide-this');
+        }
+
+        // listen for next button to be pressed
+        $('#next-button').on('click', function () {
+            $('#next-button').off('click');
+
+            // if quiz is done show results
+            if (triviaGame.unansweredQuestions === 0) {
+                triviaGame.winCondition = 'finished';
+                triviaGame.showResults();
             }
-            else if (answerArray[k] === this.answerSheet[k]) {
-                this.correctAnswers++;
-            }
+            // if not increment the question tracker and go to the next question
             else {
-                this.incorrectAnswers++;
+                triviaGame.questionTracker++;
+                triviaGame.nextQuestion(pKeys);
             }
-        }
-
-        // calculate overall grade
-        this.overallGrade = String((this.correctAnswers / 4) * 100) + '%';
-        this.showResults()
+        });
     },
 
     showResults: function () {
+        // pause timer
+        clearInterval(quizInterval);
+
         // consider win condition
-        if (this.winCondition === 'submitted') {
+        if (this.winCondition === 'finished') {
             $('#times-up').addClass('hide-this');
         }
         else {
@@ -99,6 +165,7 @@ var triviaGame = {
         }
 
         // fill scores in
+        this.overallGrade = String((this.correctAnswers / 4) * 100) + '%';
         $('#score-grade').text(this.overallGrade);
         $('#score-correct').text(this.correctAnswers);
         $('#score-incorrect').text(this.incorrectAnswers);
@@ -118,16 +185,16 @@ var triviaGame = {
         this.timeLeft = 60;
         this.correctAnswers = 0;
         this.incorrectAnswers = 0;
-        this.unansweredQuestions = 0;
+        this.unansweredQuestions = 4;
         this.overallGrade = 0;
         this.winCondition = 'none';
+        this.questionTracker = 0;
 
         $('#time-remaining').text(triviaGame.timeLeft);
+        $('#next-button').text('Next Question');
 
-        $('input[name="q1-answer"]').prop('checked', false);
-        $('input[name="q2-answer"]').prop('checked', false);
-        $('input[name="q3-answer"]').prop('checked', false);
-        $('input[name="q4-answer"]').prop('checked', false);
+        $('#congrats').removeClass('hide-this');
+        $('#times-up').removeClass('hide-this');
 
         $('#quiz-results').addClass('hide-this');
         $('#quiz-start').removeClass('hide-this');
